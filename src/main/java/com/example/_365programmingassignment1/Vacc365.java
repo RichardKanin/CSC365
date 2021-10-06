aa27962919f35e54f48d0a803200a9ddb860706c
RichardKanin <rkanin@oswego.edu>
2021-10-06 13:06:49 -0400

    This is Logan

--- src/main/java/com/example/_365programmingassignment1/Vacc365.java
+++ src/main/java/com/example/_365programmingassignment1/Vacc365.java
@@ -1,16 +1,102 @@
-package com.example._365programmingassignment1;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-
-public class Vacc365 {
-
-    public static void main(String[] args) {
-
-    }
-}
-
-
+//package com.example._365programmingassignment1;
+//
+//import java.io.*;
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.HashMap;
+//import java.util.List;
+//
+//public class main {
+//    // function for converting a list of strings into an equivalent length arraylist of strings
+//    public static ArrayList<String> converter(List<String> list) {
+//        ArrayList<String> arr = new ArrayList<String>();
+//        for (int i = 1; i < list.size(); i++) {arr.add(list.get(i));}
+//        return arr;
+//    }
+//    public static void writeToFile(List l,String name) throws FileNotFoundException{
+//        PrintWriter csvWriter = new PrintWriter("C:\\Users\\Louis\\IdeaProjects\\Assignment1_365\\src\\Data\\"+name+".csv");
+//        for (int i = 0; i < l.size(); i++) {
+//            csvWriter.write(l.get(i).toString().substring(1, l.get(i).toString().length()-1) + "\n");
+//            csvWriter.flush();
+//        }
+//        if(l.equals(null)){
+//            csvWriter.close();
+//        }
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        // File paths and variable declaration.
+//        String VAERSVax2020File = "\\C:\\Users\\richa\\Downloads\\2020VAERSVAX.csv\\";
+//        String VAERSVax2021File = "C:\\Users\\Louis\\IdeaProjects\\Assignment1_365\\src\\Data\\2021VAERSVAX.csv";
+//        String VAERSData2020File = "C:\\Users\\Louis\\IdeaProjects\\Assignment1_365\\src\\Data\\2020VAERSData.csv";
+//        String VAERSData2021File = "C:\\Users\\Louis\\IdeaProjects\\Assignment1_365\\src\\Data\\2021VAERSData.csv";
+//        String file = "\\C:\\Users\\richa\\Downloads\\2020VAERSVAX.csv\\";
+//        String file2 = "\\C:\\Users\\richa\\Downloads\\2021VAERSVAX.csv\\";
+//        String file3 = "\\C:\\Users\\richa\\Downloads\\2020VAERSData.csv\\";
+//        String file4 = "\\C:\\Users\\richa\\Downloads\\2021VAERSData.csv\\";
+//        String delimiter = ",";
+//        String line = null;
+//        String line1 = null;
+//        String line2 = null;
+//        int maximumID = 1590982;
+//        List<List<String>> lines = new ArrayList();
+//        HashMap<Integer, ArrayList<String>> IDHashTable = new HashMap<Integer, ArrayList<String>>();
+//
+//        //create an arraylist of arraylists, where each line of the CSV file is one arraylist and only
+//        //covid19 vaccinations are kept
+//        try (BufferedReader bufferedReadervax2020 = new BufferedReader(new FileReader(VAERSVax2020File))) {
+//            while ((line = bufferedReadervax2020.readLine()) != null) {
+//                String secondColumn = line.substring(8, 15);
+//                if (secondColumn.equals("COVID19")) {
+//                    List<String> values = Arrays.asList(line.split(delimiter));
+//                    lines.add(values);;
+//                }
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//        try (BufferedReader bufferedReadervax2021 = new BufferedReader(new FileReader(VAERSVax2021File))) {
+//            while ((line = bufferedReadervax2021.readLine()) != null) {
+//                String covidColumn = line.substring(8, 15);
+//                if (covidColumn.equals("COVID19")) {
+//                    List<String> values = Arrays.asList(line.split(delimiter));
+//                    lines.add(values);
+//                }
+//            }
+//
+//            for (int i = 0; i < lines.size(); i++) {
+//                int key = Integer.parseInt(lines.get(i).get(0));
+//                ArrayList<String> strObj = converter(lines.get(i));
+//                IDHashTable.put(key, strObj);
+//            }
+//            writeToFile(lines, "COVID_ONLY_DATA");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+////        lines.clear();
+//        //for reading the data files, because of commas within user reporting, we can't just
+//        //split at a delimiter. Because of this, we will take all of the data from one line
+//        //and split into two sections, first is the ID and the second is everything else.
+//
+//        try (BufferedReader bufferedReadervax2020 = new BufferedReader(new FileReader(VAERSVax2020File))) {
+//            while ((line1 = lines(1)) != null) {
+//                String firstColumnData = line1.substring(0, 6);
+//                line2 = bufferedReadervax2020.readLine();
+//                String firstColumnVax = line2.substring(0,6);
+//                while (Integer.valueOf(firstColumnData) != null || Integer.valueOf(firstColumnData) < maximumID) {
+//                    if (firstColumnData.equals(firstColumnVax)) {
+//
+//
+//                    }
+//                }
+//            }
+////                lines.clear();
+//            writeToFile(lines, "VAX_and_DATA");
+//        } catch (IOException e) {e.printStackTrace();}
+//
+//
+//    }
+//}
--- src/main/java/com/example/_365programmingassignment1/database.java
+++ src/main/java/com/example/_365programmingassignment1/database.java
@@ -10,16 +10,10 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 
-public class database extends vaersvax {
-    
-
-    public database(int vaersID, String vaxType, String vaxManu, String vaxSite, String vaxName) throws IOException {
-
-    }
+public class database {
 
     public static ArrayList<String> converter(List<String> list) {
         ArrayList<String> arr = new ArrayList<String>();
-
         for (int i = 1; i < list.size(); i++) {
             arr.add(list.get(i));
         }
@@ -30,67 +24,136 @@ public class database extends vaersvax {
     // arraylist type for vaersvax?
 
     public static void main(String[] args) throws FileNotFoundException {
-        HashMap<Integer,ArrayList<String>> hashMap = new HashMap<>();
-
+        //HashMap<Integer,ArrayList<String>> hashMap = new HashMap<>();
 
         String file = "\\C:\\Users\\richa\\Downloads\\2020VAERSVAX.csv\\";
         String file2 = "\\C:\\Users\\richa\\Downloads\\2021VAERSVAX.csv\\";
-       // String theFile = null;
+        String file3 = "\\C:\\Users\\richa\\Downloads\\2020VAERSData.csv\\";
+        String file4 = "\\C:\\Users\\richa\\Downloads\\2021VAERSData.csv\\";
         String delimiter = ",";
         String line = null;
+        String demo = null;
 
         List<List<String>> lines = new ArrayList();
+        List<List<String>> dataLines = new ArrayList();
+
 
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             while ((line = br.readLine()) != null) {
+//                System.out.println(line);
+
                 String covidColumn = line.substring(8, 15);
-                 //String line2 = line.substring(1, line.length() - 1);
                 if (covidColumn.equals("COVID19")) {
                     List<String> values = Arrays.asList(line.split(delimiter));
                     lines.add(values);
-                   //lines.remove(line2);
-                    //System.out.println(line);
-                    //System.out.println(covidColumn);
                 }
             }
-//            System.out.println(lines.get(0).get(0));
-//            ArrayList<List> arr = lines.get(0);
+//            System.out.println(lines.get(0));
+
+
+            HashMap<Integer, ArrayList> v2020COVID = new HashMap<Integer, ArrayList>();
+
             for (int i = 0; i < lines.size(); i++) {
-                // System.out.println(lines.get(i));
+                int key = Integer.parseInt(lines.get(i).get(0));
+                ArrayList<String> strObj = converter(lines.get(i));
+                v2020COVID.put(key, strObj);
             }
+            System.out.println(v2020COVID.get(902493));
+
             writeToFile(lines);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+
+        try (BufferedReader br1 = new BufferedReader(new FileReader(file3))) {
+            demo = br1.readLine();
+            ArrayList<Integer> keys = new ArrayList<>();
+            String[] data;
+            ArrayList<String> datas = new ArrayList<>();
+//            List<ArrayList> dataList = new ArrayList<>();
+            ArrayList<ArrayList> dataSet = new ArrayList<>();
+
+            HashMap<Integer, ArrayList> v2020COVIDDB = new HashMap<Integer, ArrayList>();
+            System.out.println("Here");
+            while ((demo = br1.readLine()) != null) {
+
+//                data = demo.split(",");
+//                keys.add(Integer.parseInt(data[0]));
+//                System.out.println(data[17]);
+
+                List<String> values = Arrays.asList(demo.split(","));
+                dataLines.add(values);
+
+////                System.out.println(v2020COVIDDB.get(855017));
+            }
+            for (int i = 0; i < dataLines.size(); i++) {
+                int key = Integer.parseInt(dataLines.get(i).get(0));
+                ArrayList<String> strObj = converter(dataLines.get(i));
+                v2020COVIDDB.put(key, strObj);
+            }
+            System.out.println("stop");
+            System.out.println(v2020COVIDDB.get(902493));
+//            System.out.println(dataLines.get(0));
+//            HashMap<Integer, ArrayList> v2020COVIDDB = new HashMap<Integer, ArrayList>();
+
+
+//            System.out.println(keys.size());
+//            System.out.println(dataSet.size());
+
+////            for (int i = 0; i < datas.size(); i++) {
+//                System.out.println(datas.get(1));
+////            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        // --------------------------------------------------------------
+
         try (BufferedReader br2 = new BufferedReader(new FileReader(file2))) {
             while ((line = br2.readLine()) != null) {
                 String covidColumn = line.substring(8, 15);
-//                System.out.println(line);
-                //String line2 = line.substring(1, line.length() - 1);
                 if (covidColumn.equals("COVID19")) {
                     List<String> values = Arrays.asList(line.split(delimiter));
                     lines.add(values);
                 }
             }
-            HashMap<Integer, ArrayList<String>> CovidObj = new HashMap<Integer, ArrayList<String>>();
+            HashMap<Integer, ArrayList<String>> v2021COVID = new HashMap<Integer, ArrayList<String>>();
 
             for (int i = 0; i < lines.size(); i++) {
                 int key = Integer.parseInt(lines.get(i).get(0));
-                ArrayList<String> strObj = converter(lines.get(i));
-                CovidObj.put(key, strObj);
+                ArrayList<String> strObj2 = converter(lines.get(i));
+                v2021COVID.put(key, strObj2);
+//                System.out.println(lines.get(i));
             }
+            writeToFile(lines);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (BufferedReader br3 = new BufferedReader(new FileReader(file2))) {
+            while ((line = br3.readLine()) != null) {
+                String covidColumn = line.substring(8, 15);
+                if (covidColumn.equals("COVID19")) {
+                    List<String> values = Arrays.asList(line.split(delimiter));
+                    lines.add(values);
+                }
+            }
+            HashMap<Integer, ArrayList<String>> v2021COVID = new HashMap<Integer, ArrayList<String>>();
 
-            System.out.println(CovidObj.get(904895));
-
-
-
-//            System.out.println(CovidObj.get(0905834));
+            for (int i = 0; i < lines.size(); i++) {
+                int key = Integer.parseInt(lines.get(i).get(0));
+                ArrayList<String> strObj2 = converter(lines.get(i));
+                v2021COVID.put(key, strObj2);
+//                System.out.println(lines.get(i));
+            }
             writeToFile(lines);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
    public static void writeToFile(List l) throws FileNotFoundException {
        PrintWriter csvWriter = new PrintWriter("C:\\Users\\richa\\IdeaProjects\\365ProgrammingAssignment1\\src\\main\\COVID_Only_Data.csv");
        for (int i = 0; i < l.size(); i++) {
@@ -107,12 +170,6 @@ public class database extends vaersvax {
        try (BufferedReader br7 = new BufferedReader(new FileReader(file))){
 
 
-
-
-
        }
    }
-
-
-
 }
